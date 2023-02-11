edd813b5230dd41d3318f0139acb11e099573860
Nackloose <nackloose@gmail.com>
2023-02-11 10:46:38 -0500

    no more $() or ``

--- do/update.sh
+++ do/update.sh
@@ -2,13 +2,13 @@
 
 declare -A pkg_managers=(
   ["apt"]="sudo apt update -yq && sudo apt upgrade -yq && sudo apt autoremove -y && sudo apt autoclean -y"
-  ["pacman"]="sudo pacman -Syu --noconfirm && sudo pacman -Rsn $(pacman -Qdtq) && sudo pacman -Sc --noconfirm"
+  ["pacman"]="sudo pacman -Syu --noconfirm && sudo pacman -Qdtq | xargs sudo pacman -Rsn --noconfirm && sudo pacman -Sc --noconfirm"
   ["dnf"]="sudo dnf update -y && sudo dnf autoremove -y && sudo dnf clean all"
   ["yum"]="sudo yum update -y && sudo yum autoremove -y && sudo yum clean all"
   ["zypper"]="sudo zypper refresh -y && sudo zypper update -y && sudo zypper autoremove -y && sudo zypper clean -a"
   ["brew"]="brew update --quiet && brew upgrade --quiet && brew cleanup --quiet"
   ["snap"]="sudo snap refresh"
-  ["yaourt"]="yaourt -Syu --noconfirm && yaourt -Rsn $(yaourt -Qdtq) && yaourt -Sc --noconfirm"
+  ["yaourt"]="yaourt -Syu --noconfirm && yaourt -Qdtq | xargs yaourt -Rsn && yaourt -Sc --noconfirm"
   ["flatpak"]="flatpak update --assumeyes && flatpak uninstall --unused --assumeyes && flatpak cleanup --assumeyes"
   ["gem"]="gem update && gem cleanup"
   ["entropy-client"]="entropy-client --update-all --quiet && entropy-client --autoremove --quiet && entropy-client --autoclean --quiet"
