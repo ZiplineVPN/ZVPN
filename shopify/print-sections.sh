d453932fbc14f8eef424338bbffa210a085a81e6
Nackloose <nackloose@gmail.com>
2023-02-11 11:24:53 -0500

    section parser

--- shopify/print-sections.sh
+++ shopify/print-sections.sh
@@ -5,22 +5,13 @@ parse_section() {
   section_file=$1
   
   # extract the section name
-  section_name=$(grep -oP '^\s*{%\s*section\s*(\w+)\s*%}' $section_file | awk '{print $2}')
+  section_name=$(grep -oP '"name":\s*"\w+"' $section_file | awk -F'"' '{print $4}')
   
-  # extract the block names
-  block_names=$(grep -oP '^\s*{%\s*block\s*(\w+)\s*%}' $section_file | awk '{print $2}')
-  
-  # print the section name and its blocks
+  # print the section name
   echo "$section_name"
-  for block_name in $block_names; do
-    echo "  $block_name"
-  done
 }
 
-# find all section files in the theme
-section_files=$(find . -name '*.liquid' -print0 | xargs -0 grep -l '^\s*{%\s*section\s*(\w+)\s*%}')
-
-# parse each section file
-for section_file in $section_files; do
+# iterate over all the files in the sections subfolder
+for section_file in ./sections/*; do
   parse_section $section_file
 done
