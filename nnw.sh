06c0d424c8eac88373831a1adeea09da50aab58f
Nackloose <nackloose@gmail.com>
2023-02-11 08:40:49 -0500

    typo

--- nnw.sh
+++ nnw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 gitUsername=""
 gitToken=""
-domain="https://git.nicknet.works/"
+domain="https://git.nicknet.works"
 displayName="NNW"
 slugName=$( echo "$displayName" | awk '{print tolower($0)}')
 repo="NickNetworks/$slugName"
