e5bc000aa8d391815a8830f5003c1f7b2c2c6c05
Nackloose <nackloose@gmail.com>
2023-02-18 23:07:16 -0500

    --real-run flag

--- git/rm-all-branches.sh
+++ git/rm-all-branches.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-git branch | grep -v "main" | xargs echo #git branch -D
+if [ "$1" == "--real-run" ]; then
+  real_run=true
+else
+  real_run=false
+fi
 
-# Delete all remote branches
-git branch -r | grep -v "main" | sed 's/origin\///' | xargs echo
+echo "Deleting local branches except master..."
+for branch in $(git branch | cut -c 3-); do
+  if [[ $branch != "master" ]]; then
+    if [ "$real_run" = true ]; then
+      git branch -D $branch
+      echo "Deleted local branch $branch"
+    else
+      echo "Would delete local branch $branch"
+    fi
+  fi
+done
+
+echo "Deleting remote branches except master..."
+for branch in $(git branch -r | grep -v HEAD | awk -F/ '{print $2}'); do
+  if [[ $branch != "master" ]]; then
+    if [ "$real_run" = true ]; then
+      git push origin --delete $branch
+      echo "Deleted remote branch $branch"
+    else
+      echo "Would delete remote branch $branch"
+    fi
+  fi
+done
