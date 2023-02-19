b6d24f2cfb54a73db9a29e956a6147fb1e37b33b
Nackloose <nackloose@gmail.com>
2023-02-18 22:59:09 -0500

    testing

--- git/rm-all-branches.sh
+++ git/rm-all-branches.sh
@@ -2,14 +2,10 @@
 
 # Delete all local branches
 for branch in $(git branch | cut -c 3-); do
-  if [[ $branch != "main" ]]; then
     echo "git branch -D $branch"
-  fi
 done
 
 # Delete all remote branches
 for branch in $(git branch -r | grep -v HEAD | awk -F/ '{print $2}'); do
-  if [[ $branch != "wip" ]]; then
     echo "git push origin --delete $branch"
-  fi
 done
