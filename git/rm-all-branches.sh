59022216af9a6939d5156d03ffa9a97911f9de93
Nackloose <nackloose@gmail.com>
2023-02-18 22:40:16 -0500

    update

---
+++ git/rm-all-branches.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Delete all local branches
+for branch in $(git branch | cut -c 3-); do
+  if [[ $branch != "master" ]]; then
+    git branch -D $branch
+  fi
+done
+
+# Delete all remote branches
+for branch in $(git branch -r | grep -v HEAD | awk -F/ '{print $2}'); do
+  echo "git push origin --delete $branch"
+done
