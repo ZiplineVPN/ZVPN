4bca9ed946f8358ecd5951755991f162dbfa1a41
Nackloose <nackloose@gmail.com>
2023-02-20 14:04:58 -0500

    update

--- git/gitea-automake-orgs.sh
+++ git/gitea-automake-orgs.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
-
 # Set Gitea API URL and access token
 GITEA_API_URL="https://git.nicknet.works/api/v1"
 # Prompt for Gitea API key
 read -p "Enter your Gitea API key: " GITEA_API_KEY
-
 
 # Check if --real-run was provided
 if [ $# -eq 0 ] || [ "$1" == "--real-run" ]; then
@@ -12,27 +9,26 @@ if [ $# -eq 0 ] || [ "$1" == "--real-run" ]; then
     if [ $# -eq 0 ]; then
         real_run=0
     fi
-    
+
     # Find all git repositories in the current working directory and its subdirectories
-    find . -name ".git" -type d  | while read repo
-    do
+    for repo in $(find . -name ".git" -type d); do
         # Get the path to the parent directory of the git repository
         repo_path="$(dirname "$repo")"
-        
+
         # Get the remote URL
         remote_url=$(git --git-dir="$repo" --work-tree="$repo_path" remote get-url origin 2>/dev/null)
-        
+
         # Check if the remote URL command failed
         if [ $? -ne 0 ]; then
             echo "Error: No such remote 'origin' in $repo_path."
             continue
         fi
-        
+
         # Check if the remote URL is a Gitea URL
         if [[ $remote_url =~ ^(https?:\/\/)([^\/]+)\/([^\/]+)\/([^\/]+)\/?$ ]]; then
             org_name=${BASH_REMATCH[3]}
             user_name=${BASH_REMATCH[4]}
-            
+
             # Check if the organization already exists in Gitea
             org_resp=$(curl --silent -H "Authorization: token $GITEA_ACCESS_TOKEN" -X GET "$GITEA_API_URL/orgs/$org_name")
             echo $org_repo
