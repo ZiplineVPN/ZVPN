fac12c793a0b7fc79449acaa2bd0676c40f7986c
Nackloose <nackloose@gmail.com>
2023-02-20 14:25:19 -0500

    fixes

--- git/gitea-automake-orgs.sh
+++ git/gitea-automake-orgs.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-# Set default directory to current working directory
-dir=${1:-$(pwd)}
-
 # Set Gitea API URL and access token
 GITEA_API_URL="https://git.nicknet.works/api/v1"
+
 # Prompt for Gitea API key
 read -p "Enter your Gitea API key: " GITEA_API_KEY
 
 # Check if --real-run was provided
-if [ $# -eq 0 ] || [ "$1" == "--real-run" ]; then
+if [ $# -eq 0 ] || [ "$1" == "--real-run" ] || [ "$2" == "--real-run" ]; then
+    # Set default dir to current working directory if none is provided
+    if [ $# -lt 2 ]; then
+        dir="."
+    else
+        dir="$2"
+    fi
+
     real_run=1
-    if [ $# -eq 0 ]; then
+    if [ "$1" == "--real-run" ] || [ "$2" == "--real-run" ]; then
         real_run=0
     fi
 
     # Find all git repositories in the specified directory and its subdirectories
-    for repo in $(find $dir -name ".git" -type d); do
+    for repo in $(find "$dir" -name ".git" -type d); do
         # Get the path to the parent directory of the git repository
         repo_path="$(dirname "$repo")"
 
@@ -37,14 +42,14 @@ if [ $# -eq 0 ] || [ "$1" == "--real-run" ]; then
             user_name=${BASH_REMATCH[4]}
 
             # Check if the organization already exists in Gitea
-            org_resp=$(curl --silent -H "Authorization: token $GITEA_ACCESS_TOKEN" -X GET "$GITEA_API_URL/orgs/$org_name")
+            org_resp=$(curl --silent -H "Authorization: token $GITEA_API_KEY" -X GET "$GITEA_API_URL/orgs/$org_name")
             if [[ "$org_resp" == *"user redirect does not exist"* ]]; then
                 # Create the organization in Gitea
                 if [ $real_run -eq 1 ]; then
-                    curl -H "Authorization: token $GITEA_ACCESS_TOKEN" -X POST "$GITEA_API_URL/admin/users/$user_name/orgs" -d '{"username": "'"$org_name"'"}'
+                    curl -H "Authorization: token $GITEA_API_KEY" -X POST "$GITEA_API_URL/admin/users/$user_name/orgs" -d '{"username": "'"$org_name"'"}'
                     echo "Created organization $org_name for user $user_name in $repo_path."
                 else
-                    echo "curl -H 'Authorization: token $GITEA_ACCESS_TOKEN' -X POST '$GITEA_API_URL/admin/users/$user_name/orgs' -d '{\"username\": \"$org_name\"}'"
+                    echo "curl -H 'Authorization: token $GITEA_API_KEY' -X POST '$GITEA_API_URL/admin/users/$user_name/orgs' -d '{\"username\": \"$org_name\"}'"
                 fi
             else
                 echo "Organization $org_name already exists in Gitea, skipping creation."
@@ -55,6 +60,6 @@ if [ $# -eq 0 ] || [ "$1" == "--real-run" ]; then
     done
 else
     echo "Error: Invalid argument was provided."
-    echo "Usage: $0 [--real-run] [dir]"
+    echo "Usage: $0 [dir] [--real-run]"
     exit 1
 fi
