1c4af2aa3a9df2315648551801e145d4453c1af0
Nackloose <nackloose@gmail.com>
2023-02-22 12:48:58 -0500

    fix outstanding

--- git/outstanding.sh
+++ git/outstanding.sh
@@ -9,13 +9,12 @@ for dir in $dirs; do
   local_path=$(dirname $dir)
 
   # check if the repo has any outstanding commits
-  cd $local_path
-  outstanding_commits=$(git log origin/main..HEAD)
+  outstanding_commits=$(git --git-dir=$dir log origin/main..HEAD)
 
   if [ -n "$outstanding_commits" ]; then
     # if there are outstanding commits, print repo details
-    remote_url=$(git config --get remote.origin.url)
-    branch_names=$(git branch --no-color | awk '/^\*/{print $2}')
+    remote_url=$(git --git-dir=$dir config --get remote.origin.url)
+    branch_names=$(git --git-dir=$dir branch --no-color | awk '/^\*/{print $2}')
 
     echo "Repo URL: $remote_url"
     echo "Outstanding branches: $branch_names"
