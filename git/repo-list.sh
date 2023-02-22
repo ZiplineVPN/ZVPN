d312dd0c8e6de59a3ced0c9909492eb993ae05c8
Nackloose <nackloose@gmail.com>
2023-02-21 20:57:38 -0500

    fixes and updates, add testing in repo-list

--- git/repo-list.sh
+++ git/repo-list.sh
@@ -1,46 +1,76 @@
 #!/bin/bash
 
-# Check if a root path is provided
-if [ $# -eq 0 ]; then
-  echo "Error: No root path provided."
-  echo "Usage: $0 <root_path>"
-  exit 1
-fi
+# Set Gitea API URL and access token
+GITEA_URL="https://git.nicknet.works"
+GITEA_API_URL="${GITEA_URL}/api/v1"
 
-# Find all directories that contain a .git folder inside the provided root path
-repo_directories=$(find "$1" -name ".git" -type d 2>/dev/null | sed "s/\/.git//g")
+real_run=0
 
-# Initialize an empty array to store the repo URLs
-repo_urls=()
-
-# Initialize a counter for the number of repos scanned
-count=0
-
-# Loop through each repo directory and extract the repo URL
-for repo_dir in $repo_directories; do
-  cd "$repo_dir"
-  url=$(git config --get remote.origin.url)
-  repo_urls+=("$url")
-  cd - > /dev/null
-
-  # Increment the counter and update the display
-  count=$((count + 1))
-  printf "\rNumber of repos scanned: %d" $count
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    "\"--real-run\"")
+        real_run=1
+        echo "REAL RUN! This script will actually make changes to Gitea."
+        shift
+        ;;
+    *)
+        echo "Invalid non-halting arg: '$1'"
+        shift
+        ;;
+    esac
 done
 
-# Parse the first and second directories, and hostname of each URL
-echo "\n\nList of User, Repo Names, and Hostnames: "
-for url in "${repo_urls[@]}"; do
-  # Remove the protocol from the URL (e.g., "https://")
-  url=$(echo "$url" | sed -E 's/^https?:\/\///')
+echo "Scanning repos in $(dirname "$repo")"
+if [ $real_run -eq 1 ]; then
+    echo "This is a: REAL RUN! This script WILL ACTUALLY make changes to Gitea."
+    echo "It will create organizations and repositories in Gitea."
+    echo "It will also adjust the remote URLs of the git repositories ENMASS."
+    echo "Finally, it will push the git repositories to Gitea."
+    echo "If you don't want to do this, then don't run this script with the --real-run flag."
+    echo "Ctrl+C to cancel."
+    echo
+else
+    echo "This is a DRY RUN! This script will NOT make any changes to Gitea."
+    echo "It will only print the commands that would have been run."
+    echo "If you want to make the changes to Gitea, and you are certain you're ready. Use the --real-run flag."
+    echo
+fi
 
-  # Extract the first and second directories
-  user=$(echo "$url" | cut -d '/' -f 1)
-  repo=$(echo "$url" | cut -d '/' -f 2 | sed -E 's/\.git$//')
+# Prompt for Gitea API key
+read -p "Enter your Gitea API key: " GITEA_API_KEY
 
-  # Extract the hostname
-  hostname=$(echo "$url" | cut -d '/' -f 1 | sed -E 's/^(.*)\.(.*)\.(.*)$/\1.\2/')
+# Find all git repositories in the current working directory and its subdirectories
+find . -name ".git" -type d | while IFS= read -r repo; do
+    # Get the path to the parent directory of the git repository
+    repo_path="$(dirname "$repo")"
 
-  # Display the user, repo names, and hostname
-  echo "User: $user   Repo: $repo   Hostname: $hostname"
+    # Get the remote URL
+    remote_url=$(git --git-dir="$repo" --work-tree="$repo_path" remote get-url origin 2>/dev/null)
+
+    # Check if the remote URL command failed
+    if [ $? -ne 0 ]; then
+        echo "Error: No such remote 'origin' in $repo_path."
+        continue
+    fi
+    had_echo=0
+    # Check if the remote URL is a Gitea URL
+    if [[ $remote_url =~ ^(https?:\/\/)([^\/]+)\/([^\/]+)\/([^\/]+)\/?$ ]]; then
+        org_name=${BASH_REMATCH[3]}
+        repo_name=${BASH_REMATCH[4]%.git}
+
+        #push the repo
+        if [ $real_run -eq 1 ]; then
+            git --git-dir="$repo" --work-tree="$repo_path" push --all origin
+            echo "Pushing repo $repo_name in $repo_path."
+            had_echo=1
+        else
+            echo "Push repo via: git --git-dir="$repo" --work-tree="$repo_path" push -u origin main"
+            had_echo=1
+        fi
+    else
+        echo "Remote URL $remote_url for $repo_path is not a Gitea URL, skipping."
+    fi
+    if [ $had_echo -eq 1 ]; then
+        echo
+    fi
 done
