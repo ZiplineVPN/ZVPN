64307823a3e89264854d43bf3bc57a3df221d169
Nackloose <nackloose@gmail.com>
2023-02-22 13:01:45 -0500

    test

--- git/outstanding.sh
+++ git/outstanding.sh
@@ -3,9 +3,9 @@
 # Find all directories that are git repositories, recursively
 while IFS= read -r -d '' git_dir; do
   # Check if there are any uncommitted changes
-  if [[ $(git -C "$git_dir" status --porcelain) ]]; then
-    repo_url=$(git -C "$git_dir" config --get remote.origin.url)
-    branches_with_commits=$(git -C "$git_dir" branch --list --no-color | while read branch; do git -C "$git_dir" log --oneline "$branch" | grep -q . && echo "$branch"; done)
+  if [[ $(git --git-dir="$git_dir/.git" --work-tree="$git_dir" status --porcelain) ]]; then
+    repo_url=$(git --git-dir="$git_dir/.git" --work-tree="$git_dir" config --get remote.origin.url)
+    branches_with_commits=$(git --git-dir="$git_dir/.git" --work-tree="$git_dir" branch --list --no-color | while read branch; do git --git-dir="$git_dir/.git" --work-tree="$git_dir" log --oneline "$branch" | grep -q . && echo "$branch"; done)
 
     # Print the repository URL, branches with outstanding commits, and local path to the repo
     printf "Repo URL: %s\nBranches with outstanding commits: %s\nPath: %s\n\n" "$repo_url" "$branches_with_commits" "$git_dir"
