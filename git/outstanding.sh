5cd3999c9d6952a280c1f33572acfded085f3cfc
Nackloose <nackloose@gmail.com>
2023-02-22 12:57:45 -0500

    try new paradigm

--- git/outstanding.sh
+++ git/outstanding.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-# find all directories containing a .git subdirectory
-dirs=$(find . -name ".git" -type d -prune)
+# Find all directories that are git repositories, recursively
+while IFS= read -r -d '' git_dir; do
+  # Check if there are any uncommitted changes
+  if [[ $(git -C "$git_dir" status --porcelain) ]]; then
+    repo_url=$(git -C "$git_dir" config --get remote.origin.url)
+    branches_with_commits=$(git -C "$git_dir" branch --list --no-color | while read branch; do git -C "$git_dir" log --oneline "$branch" | grep -q . && echo "$branch"; done)
 
-# iterate over each repo directory
-for dir in $dirs; do
-  # extract the parent directory (the repo's local path)
-  local_path=$(dirname $dir)
-
-  # check if the repo has any outstanding commits
-  outstanding_commits=$(git --git-dir=$dir log origin/main..HEAD)
-
-  if [ -n "$outstanding_commits" ]; then
-    # if there are outstanding commits, print repo details
-    remote_url=$(git --git-dir=$dir config --get remote.origin.url)
-    branch_names=$(git --git-dir=$dir branch --no-color | awk '/^\*/{print $2}')
-
-    echo "Repo URL: $remote_url"
-    echo "Outstanding branches: $branch_names"
-    echo "Local path: $local_path"
+    # Print the repository URL, branches with outstanding commits, and local path to the repo
+    printf "Repo URL: %s\nBranches with outstanding commits: %s\nPath: %s\n\n" "$repo_url" "$branches_with_commits" "$git_dir"
   fi
-done
+done < <(find . -name ".git" -type d -prune -print0)
