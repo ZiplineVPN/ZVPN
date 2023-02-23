11931be5c7228981dc2627f57167dd46140d00d0
Nackloose <nackloose@gmail.com>
2023-02-23 03:12:03 -0500

    test

--- check/wg-running.sh
+++ check/wg-running.sh
@@ -5,7 +5,7 @@ systemctl is-active --quiet "wg-quick@${SERVER_WG_NIC}"
 if [[ $? -ne 0 ]]; then
     echoc cyan "$(color yellow "Wireguard") does $(color red "NOT appear") to be running."
     echoc red "You can check if WireGuard is running with: systemctl status wg-quick@${SERVER_WG_NIC}"
-    echoc bright_red "If you get something like \"Cannot find device ${SERVER_WG_NIC}\", please reboot!"
+    echoc red_bright "If you get something like \"Cannot find device ${SERVER_WG_NIC}\", please reboot!"
     exit 1
 else 
     exit 0
