5ce42bea1782a811ae79f5916e3d4894271e9a27
Nackloose <nackloose@gmail.com>
2023-02-23 02:51:03 -0500

    test

--- check/wg-install.sh
+++ check/wg-install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 bolors=$(curl -kLSs https://git.nicknet.works/Nackloose/Bolors/raw/branch/main/bolors.sh)
-echo $bolors
 eval "$bolors"
-color green "Checking for Wireguard..."
+echoc green "Checking for Wireguard..."
 if [[ -e /etc/wireguard/params ]]; then
     echo "Wireguard Installed."
     exit 0
