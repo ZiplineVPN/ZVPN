13ed435ae738ce02c75cb9726c6ae8ef1ec907f5
Nackloose <nackloose@gmail.com>
2023-02-23 03:00:58 -0500

    update wg-running

--- check/wg-running.sh
+++ check/wg-running.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+bolors=$(curl -kLSs https://git.nicknet.works/Nackloose/Bolors/raw/branch/main/bolors.sh)
+eval "$bolors"
 systemctl is-active --quiet "wg-quick@${SERVER_WG_NIC}"
 if [[ $? -e 0 ]]; then
-    echo -e "\n${RED}WARNING: WireGuard does not seem to be running.${NC}"
-    echo -e "${ORANGE}You can check if WireGuard is running with: systemctl status wg-quick@${SERVER_WG_NIC}${NC}"
-    echo -e "${ORANGE}If you get something like \"Cannot find device ${SERVER_WG_NIC}\", please reboot!${NC}"
+    echoc cyan "$(color yellow "Wireguard") does $(color red "NOT appear") to be running."
+    echoc orange "You can check if WireGuard is running with: systemctl status wg-quick@${SERVER_WG_NIC}"
+    echoc red "If you get something like \"Cannot find device ${SERVER_WG_NIC}\", please reboot!"
     exit 1
 else 
     exit 0
