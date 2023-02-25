c1d4709f45b22245b58a4e5736a759fc10eb8a03
Nackloose <nackloose@gmail.com>
2023-02-25 13:27:48 -0500

    env updates

--- includes/zvpn-vars.sh
+++ includes/zvpn-vars.sh
@@ -1,3 +1,11 @@
+serverAndCertCheck() {
+    if [[ -e /etc/wireguard/params && -e /root/wg0-client-default.conf ]]; then
+        echo "true"
+    else
+        echo "false"
+    fi
+}
+
 VPS_IP=$(ip -4 addr | sed -ne 's|^.* inet \([^/]*\)/.* scope global.*$|\1|p' | awk '{print $1}' | head -1)
 VPS_NIC="$(ip -4 route ls | grep default | grep -Po '(?<=dev )(\S+)' | head -1)"
 WG_NIC="wg0"
@@ -7,3 +15,4 @@ WG_DNS1="8.8.8.8"
 WG_DNS2="8.8.4.4"
 WG_PORT=$(shuf -i49152-65535 -n1)
 WG_ENDPOINT="${VPS_IP}:${WG_PORT}"
+WG_INSTALLED=$(serverAndCertCheck)
