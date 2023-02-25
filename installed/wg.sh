599c1469a28d719ce5e50ddfed75f96298b7a9b2
Nackloose <nackloose@gmail.com>
2023-02-25 13:42:24 -0500

    updates to wg install

--- installed/wg.sh
+++ installed/wg.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-echoc cyan "Checking for $(color yellow Wireguard)..."
-if [[ -e /etc/wireguard/params ]]; then
-    echoc yellow "Wireguard $(color green "Installed!")"
-    exit 0
-fi
-echoc yellow "Wireguard $(color red "NOT Installed!")"
-exit 1
+echo $WG_INSTALLED
